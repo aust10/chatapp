c65dd723a231c4dda72eb9c5a95e77554ab81d40
austen cote <coteausten@gmail.com>
2020-06-19 13:52:01 -0700

    updating

---
+++ client-react/build/precache-manifest.a6fe2a6db2436680a025e66dcf546154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410630c110163842a73659f2736090b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35a1fa04847c2971f411",
+    "url": "/static/css/main.cab7c866.chunk.css"
+  },
+  {
+    "revision": "8e5fa54c88d79c0c9ea0",
+    "url": "/static/js/2.b3abde8c.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.b3abde8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a1fa04847c2971f411",
+    "url": "/static/js/main.fa14cbd7.chunk.js"
+  },
+  {
+    "revision": "8c06afd21f2092bd82d0",
+    "url": "/static/js/runtime-main.7457ec29.js"
+  }
+]);
